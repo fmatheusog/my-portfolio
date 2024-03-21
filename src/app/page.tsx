0da491cff411f72375be575e0d83662baeb4b22c
Felipe Matheus <fmatheusog@gmail.com>
2024-03-21 11:24:16 -0300

    feat: implements home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center py-24">
+      <section className="flex flex-col gap-[10px] mb-[62px] rounded mx-auto py-5 w-full">
+        <motion.div
+          className="w-full h-fit flex justify-start px-4"
+          initial={{ x: -600 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1.3 }}
+        >
+          <h1 className="text-3xl font-bold">
+            Felipe Matheus - FullStack Developer
+          </h1>
+        </motion.div>
+        <motion.div
+          className="w-full px-4"
+          initial={{ x: 400 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1.3 }}
+        >
+          <h2 className="text-lg">
+            Lorem ipsum dolor sit amet consectetur
+            adipisicing elit.
+          </h2>
+        </motion.div>
+      </section>
+
+      <motion.p
+        className="px-[10px] w-full"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Voluptatibus maxime odio quo, vitae
+        repudiandae possimus quia laborum ipsum a cum
+        blanditiis beatae tempora aspernatur magni
+        dignissimos voluptatum dicta assumenda rerum!
+      </motion.p>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
+    colors: {
+      // light
+      primary: '#F4AFAB',
+      secondary: '#F4CBC6',
+      tertiary: '#F7EDF0',
+      vanilla: '#F4EEA9',
+      icterine: '#F4F482',
+      // dark
+      primary_dark: '#0B132B',
+      secondary_dark: '#1C2541',
+      tertiary_dark: '#3A506B',
+      verdigris: '#5BC0BE',
+      white: '#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
