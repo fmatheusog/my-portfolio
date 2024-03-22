23246fd9dc41351e7310ab0dfcb086233cea0c24
Felipe Matheus <fmatheusog@gmail.com>
2024-03-22 15:05:03 -0300

    refactor: reimplements ThemeSliderToggle component

--- src/components/button/specifics/ThemeSliderToggle/ThemeSliderToggle.tsx
+++ src/components/button/specifics/ThemeSliderToggle/ThemeSliderToggle.tsx
@@ -1,6 +1,5 @@
 // import styles from './ThemeSliderToggle.module.css';
 
-import { motion } from 'framer-motion';
 import { FiSun, FiMoon } from 'react-icons/fi';
 
 import ThemeSliderToggleStyle from '../../../../../styles/components/button/specifics/ThemeSliderToggleStyle';
@@ -21,51 +20,31 @@ const ThemeSliderToggle: React.FC<
   useEffect(() => setMounted(true), []);
   if (!mounted) return null;
 
-  function isDark() {
-    return theme === 'dark';
-  }
-
   return (
-    <div className="relative flex w-fit items-center rounded-full">
-      <button
-        className={`${ThemeSliderToggleStyle} ${
-          theme === 'light'
-            ? 'text-white'
-            : 'text-slate-300'
-        }`}
-        onClick={() => {
-          setTheme(isDark() ? 'light' : 'dark');
-        }}
-      >
-        <FiMoon className="relative z-10 text-lg md:text-sm" />
-        <span className="relative z-10">Light</span>
-      </button>
-      <button
-        className={`${ThemeSliderToggleStyle} ${
-          theme === 'dark' ? 'text-white' : 'text-slate-800'
-        }`}
-        onClick={() => {
-          setTheme(isDark() ? 'light' : 'dark');
-        }}
-      >
-        <FiSun className="relative z-10 text-lg md:text-sm" />
-        <span className="relative z-10">Dark</span>
-      </button>
-      <div
-        className={`absolute inset-0 z-0 flex ${
-          theme === 'dark' ? 'justify-end' : 'justify-start'
-        }`}
-      >
-        <motion.span
-          layout
-          transition={{
-            type: 'spring',
-            damping: 15,
-            stiffness: 250,
+    <div
+      className={`flex items-center ${
+        theme === 'light' ? 'justify-start' : 'justify-end'
+      } rounded-full w-[60px] h-[20px] overflow-visible bg-white`}
+    >
+      {theme === 'dark' ? (
+        <button
+          className={`${ThemeSliderToggleStyle} bg-secondary_dark text-white`}
+          onClick={() => {
+            setTheme('light');
           }}
-          className="h-full w-1/2 rounded-full bg-gradient-to-r from-violet-600 to-indigo-600"
-        />
-      </div>
+        >
+          <FiMoon className="z-10 text-lg md:text-sm" />
+        </button>
+      ) : (
+        <button
+          className={`${ThemeSliderToggleStyle} bg-icterine text-secondary_dark`}
+          onClick={() => {
+            setTheme('dark');
+          }}
+        >
+          <FiSun className="z-10 text-lg md:text-sm" />
+        </button>
+      )}
     </div>
   );
 };
